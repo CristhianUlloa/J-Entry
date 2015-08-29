ff794e58acc04226eecead265824e9b86ca7b50f
Tricia Divita <tdivita@mit.edu>
2015-08-29 06:37:59 -0700

    Made multi-page navigation work! Added dummy photos page.

--- j-entry-page/j-entry-page.js
+++ j-entry-page/j-entry-page.js
@@ -1,5 +1,5 @@
 if (Meteor.isClient) {
-  // counter starts at 0
+/*  // counter starts at 0
   Session.setDefault('counter', 0);
 
   Template.hello.helpers({
@@ -13,11 +13,31 @@ if (Meteor.isClient) {
       // increment the counter when button is clicked
       Session.set('counter', Session.get('counter') + 1);
     }
-  });
+  });*/
+
+// Trying to fix top nav behavior
+/*  Template.topnav.events({
+    "click .nav a":function(event, template){
+      console.log("clicked a thing");
+      template.$(".active").removeClass(active);                // Does not work from here
+      event.currentTarget.addClass("active");
+    }
+  });*/
 }
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
+/*  Meteor.startup(function () {
     // code to run on server at startup
-  });
+  });*/
 }
+
+Router.route("/", {
+    template: "about"
+});
+Router.route("/about");
+Router.route("/photos");
+
+// $(".nav a").on("click", function(){
+//    $(".nav").find(".active").removeClass("active");
+//    $(this).parent().addClass("active");
+// });
