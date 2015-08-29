679e2a9842a01a3bb8646ce77b50731da61cab70
Tricia Divita <tdivita@mit.edu>
2015-08-29 12:28:39 -0700

    Added jquery. Added initial photo gallery. Has dummy pictures and needs some picture sizing help to not be stupid. But arrow navigation works in the modal! :D

--- j-entry-page/j-entry-page.js
+++ j-entry-page/j-entry-page.js
@@ -23,6 +23,56 @@ if (Meteor.isClient) {
       event.currentTarget.addClass("active");
     }
   });*/
+
+  Template.photos.events({
+    "click .gallery-list li img":function(event, template){
+      var src = event.currentTarget.src;
+      var img = '<img src="' + src + '" class="img-responsive"/>';
+
+      var index = $(event.currentTarget).parent("li").index();
+      var html = "";
+      html += img;
+      html += '<div style="display:none;">';
+      html += '<a class="controls previous" href="' + (index) + '">&laquo; prev</a>';
+      html += '<a class="controls next" href="'+ (index+2) + '">next &raquo;</a>';
+      html += '</div>';
+
+      $("#photo-modal").modal();
+      $("#photo-modal").on("shown.bs.modal", function(){
+        $("#photo-modal .modal-body").html(html);
+      });
+      $("#photo-modal").on("hidden.bs.modal", function(){
+        $("#photo-modal .modal-body").html("");
+      });
+    },
+
+    "keydown":function(event, template){
+      if (event.keyCode == 37){ /*left arrow */
+        var href = $(".previous")[0].href;
+        var index = href.substr(href.lastIndexOf('/') + 1);
+        var src = $("ul.row li:nth-child("+ index +") img")[0].src;
+        $(".modal-body img").attr("src", src);
+
+        var newPrevIndex = Math.max(1, parseInt(index) - 1);
+        var newNextIndex = Math.min($("#main-gallery li").length, parseInt(newPrevIndex) + 2);
+         
+        $(".previous").attr('href', newPrevIndex);
+        $('.next').attr('href', newNextIndex);
+      }
+      if (event.keyCode == 39){ /*right arrow */
+        var href = $(".next")[0].href;
+        var index = href.substr(href.lastIndexOf('/') + 1);
+        var src = $("ul.row li:nth-child("+ index +") img")[0].src;
+        $(".modal-body img").attr("src", src);
+
+        var newPrevIndex = Math.max(1, parseInt(index) - 1);
+        var newNextIndex = Math.min($("#main-gallery li").length, parseInt(newPrevIndex) + 2);
+         
+        $(".previous").attr('href', newPrevIndex);
+        $('.next').attr('href', newNextIndex);
+      }
+    }
+  })
 }
 
 if (Meteor.isServer) {
@@ -41,3 +91,17 @@ Router.route("/photos");
 //    $(".nav").find(".active").removeClass("active");
 //    $(this).parent().addClass("active");
 // });
+
+/*$(document).ready(function(){
+  $('li img').on('click',function(){
+    var src = $(this).attr('src');
+    var img = '<img src="' + src + '" class="img-responsive"/>';
+    $('#photo-modal').modal();
+    $('#photo-modal').on('shown.bs.modal', function(){
+      $('#photo-modal .modal-body').html(img);
+    });
+    $('#photo-modal').on('hidden.bs.modal', function(){
+      $('#photo-modal .modal-body').html('');
+    });
+  });
+})*/
